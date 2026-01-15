c4dfb1038af2b83f806443b0a497da973cf59dfb
Talha Ramazan Köse <talharamazanks@gmail.com>
2026-01-15 05:30:01 +0300

    test-logs: auto commit @ 2026-01-15 05:30:01

---
+++ fire_image_watcher.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+REPO_DIR="/opt/tapo"
+WATCH_DIR="/opt/tapo/detections"
+LOCK_FILE="/tmp/fire_git.lock"
+
+cd "$REPO_DIR"
+
+inotifywait -m -e create --format "%f" "$WATCH_DIR" | while read FILE
+do
+  # Sadece resimler
+  if [[ "$FILE" =~ \.(jpg|jpeg|png)$ ]]; then
+
+    # Cron ile çakışmayı önle
+    (
+      flock -n 9 || exit 0
+
+      TS=$(date "+%Y-%m-%d %H:%M:%S")
+
+      git add .
+      git commit -m "🔥 FIRE DETECTED: $FILE @ $TS" || exit 0
+      git push
+
+    ) 9>"$LOCK_FILE"
+
+  fi
+done
